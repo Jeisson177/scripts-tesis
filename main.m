4c2bf924ea2ab51f259fe34ed90c8e8e9e981b7d
Jeisson177 <79467624+Jeisson177@users.noreply.github.com>
2024-05-31 12:21:28 -0500

    completado curvas

--- main.m
+++ main.m
@@ -17,17 +17,22 @@ Ida4104 = [4.587917000000000, -74.149976900000000];
 Vuelta4104 = [4.562243400000000, -74.083503800000000];
 
 %% importar solo un dato
-Sensor=ImportarDatos.Sensor('Datos\2024-04-22\4104');
+Sensor=ImportarDatos.Sensor('Datos\2024-04-23\4020');
 datosCordenadasSensor=ImportarDatos.SensorCordenadas(Sensor);
-tiempoR=Calculos.Ruta(datosCordenadasSensor,Ida4104,Vuelta4104,20);
-T=size(tiempoR);
-Ccurvas1=Calculos.Lcurvasida4020();
-Ccurvas2=Calculos.LcurvasVuelta4020();
-Ccurvas3=Calculos.Lcurvasida4104();
-Ccurvas=Calculos.LcurvasVuelta4104();
-for i=1:T(1)
-array2(:,i)=Calculos.riesgoCurva2(datosCordenadasSensor,tiempoR{i,2},tiempoR{i,3},Ccurvas);
-end
+fechaInicio="2024-04-23 6:39:00.434";
+ fechaFin="2024-04-23 7:50:00.434";
+r=Calculos.riesgoCurva(datosCordenadasSensor,fechaInicio, fechaFin);
+
+%tiempoR=Calculos.Ruta(datosCordenadasSensor,Ida4104,Vuelta4104,20);
+%T=size(tiempoR);
+
+% Ccurvas1=Calculos.Lcurvasida4020();
+% Ccurvas2=Calculos.LcurvasVuelta4020();
+% Ccurvas3=Calculos.Lcurvasida4104();
+% Ccurvas=Calculos.LcurvasVuelta4104();
+% for i=1:T(1)
+% array2(:,i)=Calculos.riesgoCurva2(datosCordenadasSensor,tiempoR{i,2},tiempoR{i,3},Ccurvas);
+% end
 % for i=1:T(1)
 %     m{i}=Map.Ruta(datosCordenadasSensor,tiempoR{i,1},tiempoR{i,2},'r','ida','ida');
 %     m2{i}=Map.Ruta(datosCordenadasSensor,tiempoR{i,2},tiempoR{i,3},'b','vuelta','vuelta');
@@ -54,14 +59,15 @@ for i = 1:5
 end
 
 
+
 %%
 nombres = fieldnames(datosBuses);
 narray = 1;
-for i=1:5
+for i=6:10
     Na = nombres{i};  % Asegurarse de que Na sea una cadena de texto
-    data=datosBuses.(Na).bus_4104.EV19;
-    tiempos=datosBuses.(Na).bus_4104.tiempoRuta;
-    Ti = size(datosBuses.(Na).bus_4104.tiempoRuta);
+    data=datosBuses.(Na).bus_4020.EV1;
+    tiempos=datosBuses.(Na).bus_4020.tiempoRuta;
+    Ti = size(datosBuses.(Na).bus_4020.tiempoRuta);
     for j=1:Ti(1)
         fechaInicio= tiempos{j, 1};
         fechaFin=tiempos{j, 3};
@@ -73,24 +79,24 @@ for i=1:5
                 fechaFin = datetime(fechaFin, 'InputFormat', 'yyyy-MM-dd HH:mm:ss.SSS');
             end
         Filtrados =data(data.fechaHoraLecturaDato >= fechaInicio & data.fechaHoraLecturaDato <= fechaFin, :);%se filtran por fecha
-        F1 = Filtrados(Filtrados.codigoComportamientoAnomalo == '1', :);
-F2 = Filtrados(Filtrados.codigoComportamientoAnomalo == '2', :);
-F3 = Filtrados(Filtrados.codigoComportamientoAnomalo == '3', :);
-F4 = Filtrados(Filtrados.codigoComportamientoAnomalo == '4', :);
+%         F1 = Filtrados(Filtrados.codigoComportamientoAnomalo == '1', :);
+% F2 = Filtrados(Filtrados.codigoComportamientoAnomalo == '2', :);
+% F3 = Filtrados(Filtrados.codigoComportamientoAnomalo == '3', :);
+% F4 = Filtrados(Filtrados.codigoComportamientoAnomalo == '4', :);
 
-        Evento18=size(F1);
+        Evento18=size(Filtrados);
             
-            E1(1,narray)=Evento18(1);
-            Evento18=size(F2);
-            
-            E2(1,narray)=Evento18(1);
-            
-            Evento18=size(F3);
-            
-            E3(1,narray)=Evento18(1);
-            Evento18=size(F4);
-            
-            E4(1,narray)=Evento18(1);
+             E1(1,narray)=Evento18(1);
+%             Evento18=size(F2);
+%             
+%             E2(1,narray)=Evento18(1);
+%             
+%             Evento18=size(F3);
+%             
+%             E3(1,narray)=Evento18(1);
+%             Evento18=size(F4);
+%             
+%             E4(1,narray)=Evento18(1);
             
 %             E(:,narray)=sum(Filtrados.estadoAperturaCierrePuertas);
             
@@ -378,6 +384,13 @@ HoraFinal  = '2024-04-23 6sae:40:00.434';
 %mis = Map.Ruta(datosCordenadasSensor, HoraInicio, HoraFinal, 'b-',"titulo","ruta");
 a=Calculos.riesgoCurva(datosCordenadasSensor, HoraInicio, HoraFinal);
 %mis = Map.Ruta(datosCordenadasSensor, HoraFinal, '2024-04-22 6:20:45.434','r-', mis)
+
+%%
+d=datosBuses.f_2024_04_22.bus_4104.datosSensor;
+t1=datosBuses.f_2024_04_22.bus_4104.tiempoRuta{1, 1};
+t2=datosBuses.f_2024_04_22.bus_4104.tiempoRuta{1, 2};
+map=Map.Ruta(d,t1,t2,'b-',"titulo4020 semana 1","ruta");
+
 
 %%
 
