2c16ab0a76d11bafd805a4616cd944fcd69a2106
d-jaramillo <138623643+d-jaramillo@users.noreply.github.com>
2025-03-10 10:42:14 -0500

    Reunión 10 marzo

---
+++ analisis_aceleraciones.m
@@ -0,0 +1,11 @@
+%% Gráfico de magnitudes vs duraciones de aceleraciones y desaceleraciones
+figure
+i = 8;
+scatter(datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 3},datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 1})
+hold on, grid
+scatter(datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 4},datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 2})
+scatter(datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 7},datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 5})
+scatter(datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 8},datosBuses.bus_4012.f_03_07_2024.indicesAceleracionRuta{i, 6})
+
+%% Cálculo de aceleraciones y desaceleraciones por km
+cellfun(@length,datosBuses.bus_4012.f_04_07_2024.indicesAceleracionRuta)./repmat(datosBuses.bus_4012.f_04_07_2024.tiempoRuta.Kilometros_Ida,1,8)
